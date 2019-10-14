13ba51c53472b0f7ab23833492daf869c8f7a1ed
syafmz <syafiqahzailani2694@gmail.com>
2019-10-15 02:49:36 +0800

    Update
    Edit active headers

--- js/content.js
+++ js/content.js
@@ -1,18 +1,46 @@
-var i, content;
-content = document.getElementsByClassName("content");
+var i;
+var content = document.getElementsByClassName("content");
+var nav = {about:0, portfolio:1, resume:2, contact:3};
+var menu = document.getElementById("menu");
+var li = menu.getElementsByClassName("btn");
+var path = window.location.href.split('#');
 
-for (i = 1; i < content.length; i++) 
-{
-  content[i].style.display = "none";
-}
+function resetContent()
+{  
+  var current = document.getElementsByClassName("active");
 
-function openContent(pageName) 
-{
+  // If there's no active class
+  if (current.length > 0) 
+  {
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  
   for (i = 0; i < content.length; i++) 
   {
     content[i].style.display = "none";
   }
+}
 
-  // Show the specific tab content
+function showContent(pageName, n)
+{
   document.getElementById(pageName).style.display = "block";
+  li[n].className += " active";
+}
+
+function openContent(pageName, n) 
+{
+  resetContent();
+  showContent(pageName, n);
+}
+
+resetContent();
+
+if(typeof path[1] != 'undefined' && path[1])
+{
+  var myContent = path[1];
+  showContent(myContent, nav[myContent]);  
+}
+else
+{
+  showContent('about', nav["about"]);
 }
