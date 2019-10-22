bce4967ec4b6a308a5d6ddda10bc730a97d0130e
syafmz <syafiqahzailani2694@gmail.com>
2019-10-23 03:43:47 +0800

    Update
    Edit footer and resume

--- js/plugins.js
+++ js/plugins.js
@@ -2,4 +2,4 @@
 (function(){var e;var d=function(){};var b=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeline","timelineEnd","timeStamp","trace","warn"];var c=b.length;var a=(window.console=window.console||{});while(c--){e=b[c];if(!a[e]){a[e]=d}}}());
 
 // Place any jQuery/helper plugins in here.
-$(window).load(function(){$(".preloader").fadeOut(1000, "linear")});
+$(window).load(function(){$(".preloader").fadeOut("slow", "linear")});
