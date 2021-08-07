1c3627aa28e813baedb3dbca032df0f532b6158e
akulahalfi <akulahalfi@aol.com>
2021-08-07 23:10:14 +0800

    Migration

--- js/copyright.js
+++ js/copyright.js
@@ -1,2 +1,1 @@
 var start=new Date();var today=new Date();start.setFullYear(2011);if(today.getFullYear()==start.getFullYear()){document.write(today.getFullYear())}else{document.write(start.getFullYear()+"-"+today.getFullYear())};
-
